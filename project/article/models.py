0235c3c1ae1bc86112c7eabb06b5272a03043cf1
jcusestabs <josecarlosramirezperalta@gmail.com>
2021-04-11 00:14:48 -0600

    Created model 'article' and registered into admin

--- project/article/models.py
+++ project/article/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Article(models.Model):
+    author              = models.OneToOneField(settings.AUTH_USER_MODEL ,on_delete=models.CASCADE)
+    creation_date       = models.DateField(null=False, blank=False)
+    last_edit_date      = models.DateField(null=False, blank=False)
+    title               = models.CharField(null=False, blank=False, max_length=50)
+    content             = models.TextField(null=False, blank=False, max_length=2000)
+    
